71a2d2c7b8d4f1169860bb35c0b4665d69bef770
McRae <mpetrey@gmail.com>
2019-06-06 09:23:57 -0400

    feat: set up server

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const path = require('path');
+
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+const port = 3000;
+
+app.listen(port, function() {
+  console.log('Server is listening on port 3000!');
+});
